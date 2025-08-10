bc00c669093860535e9da31440765f42797b0a1a
Flapchik <58729268+Flapchik@users.noreply.github.com>
2025-08-10 08:52:39 +0300

    Фикс response -> followup x3

--- views/nutrients_view.py
+++ views/nutrients_view.py
@@ -69,7 +69,8 @@ class OtherServicesView(View):
                             f"Ваш баланс: {await DonationCRUD.get_tk(discord_id)} ТC",
                 color=discord.Color.red()
             )
-            await interaction.followup.edit_message(interaction.message.id, embed=error_embed, view=self.shop_view)
+            await interaction.followup.edit_message(message_id=interaction.message.id, embed=error_embed,
+                                                    view=self.shop_view)
             return False
 
         result = await DonationCRUD.remove_tk(interaction.user.id, total_price)
